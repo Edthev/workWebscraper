69e5b925362ae6a1fa22c180460546d93e7601b6
Bronx Agent <tbronx@asdenproperties.com>
2024-06-25 16:39:06 -0400

    reinit vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FormComponent from "../components/form.jsx"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        tests1234
+        <FormComponent name="TestProp1" link="#"/>
+      </div>
+    </>
+  )
+}
+
+export default App
