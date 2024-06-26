77e1a74b8ba5dd6fc2cbb1d3362e5f79431387e2
Edward <edthevargas@gmail.com>
2024-06-26 16:39:31 -0400

    added form component and functions to handle submitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
 import FormComponent from "../components/form.jsx"
+import react, {useState} from 'react'
 
 function App() {
-
+  const [formData,setFormData] = useState({
+    names:''
+  })
+  const handleChange = (e) =>{
+    const {names,value} = e.target;
+    setFormData({...formData, [names]:value})
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setFormData({names:""})
+  }
   return (
     <>
       <div>
         tests1234
-        <FormComponent name="TestProp1" link="#"/>
+        <form>
+          <FormComponent name="input"  isRequired={true} />
+          <div></div>
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
     </>
   )
