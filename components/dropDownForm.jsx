20f3a19aeb51708947ecbbd216d1c58f14cddec8
Edward <edthevargas@gmail.com>
2024-07-09 11:24:10 -0400

    init drop down component

---
+++ components/dropDownForm.jsx
@@ -0,0 +1,18 @@
+const DropDownFormComponent = ({name,type,isRequired, options}) => {
+
+
+    return (
+        <>
+            <label>Choose a {name}: </label>
+            <select className="dropdown" name="dropdown">
+                {options.map((option,i)=>(
+                    <option key={i} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </>
+    )
+}
+
+export default DropDownFormComponent
