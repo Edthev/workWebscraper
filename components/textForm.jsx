d8048822f70fba1179c5a202ebd2173c8e8df735
Edward <edthevargas@gmail.com>
2024-07-15 13:44:30 -0400

    input data is now displayed

--- components/textForm.jsx
+++ components/textForm.jsx
@@ -1,14 +1,28 @@
+import react, {useState} from 'react'
+
 const textForm = ({name,type,isRequired,placeholder}) => {
+    const [formData,setFormData] = useState("")
+
+      const handleChange = (e) =>{
+          const {value} = e.target;
+          console.log("value",value)
+        setFormData(value)
+        console.log("formData",formData)
+      }
     return (
         <>
             <label>{name}</label>
-            <input 
-                type={type} 
-                id={name} 
-                name={name} 
-                {...{isRequired} == true ? Required :{}}
-                placeholder={placeholder}
-            />
+            <div class="">
+                <input 
+                    type={type} 
+                    id={name} 
+                    name={name} 
+                    {...{isRequired} == true ? Required :{}}
+                    placeholder={placeholder}
+                    onChange={handleChange}
+                />
+            <p>{formData}</p>
+            </div> 
         </>
     )
 }
