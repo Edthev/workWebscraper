521f0798f6507eaea7507e50572f98c0b638442f
Edward <edthevargas@gmail.com>
2024-07-15 12:01:24 -0400

    set prevent default and commented out handleChange for fixing

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,26 @@ function App() {
   const [formData,setFormData] = useState({
     names:''
   })
-  const handleChange = (e) =>{
-    const {names,value} = e.target;
-    setFormData({...formData, [names]:value})
-  }
+  // const handleChange = (e) =>{
+  //   const {names,value} = e.target;
+  //   setFormData({...formData, [names]:value})
+  // }
   const handleSubmit = (e) =>{
     e.preventDefault();
-    setFormData({names:""})
+    // setFormData({names:""})
   }
   return (
       <div>
         <SideBar/>
         Asden Maintenance 
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextFormComponent name="Input Name:"  isRequired={false} placeholder="Name"/>
           <DropDownFormComponent name="Address"  isRequired={true} options={buildingAddressesArray}/>
           <TextFormComponent name="Input Apt:"  isRequired={true} placeholder="Apt #"/>
           <TextFormComponent name="Input Notes:"  isRequired={false} placeholder="Notes"/>
           <DropDownFormComponent name="Status"  isRequired={false} options={status}/>
           <div></div>
-          <input type="submit" value="Submit"/>
+          <input type="submit" value="Submit" />
         </form>
       </div>
   )
