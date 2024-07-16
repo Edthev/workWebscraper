2626ca524a6331303c6571d773fb642917b84763
Edward <edthevargas@gmail.com>
2024-07-16 12:01:42 -0400

    changed variable name

--- components/textForm.jsx
+++ components/textForm.jsx
@@ -1,21 +1,21 @@
 import react, {useState} from 'react'
 
-const textForm = ({name,type,isRequired,placeholder,handleChange}) => {
+const textForm = ({title,type,isRequired,placeholder,handleChange}) => {
     const [formData,setFormData] = useState("")
 
       const handlingChange = (e) =>{
-        const names = name
-        const values = e.target.value
-        handleChange(names,values)
+        const componentTitle = title
+        const changeValue = e.target.value
+        handleChange(componentTitle,changeValue)
       }
     return (
         <>
-            <label>{name}</label>
+            <label>Input {title}:</label>
             <div>
                 <input 
                     type={type} 
-                    id={name} 
-                    name={name} 
+                    id={title} 
+                    name={title} 
                     {...{isRequired} == true ? Required :{}}
                     placeholder={placeholder}
                     onChange={handlingChange}
