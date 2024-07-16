0da6f31b3d9882de3c1f999b9d86a759d541c949
Edward <edthevargas@gmail.com>
2024-07-16 11:56:02 -0400

    handle child component changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,19 @@ function App() {
   const vendorArray = Object.keys(Object.values(data["vendors"])[0])
   const status = data["status"]
   const [formData,setFormData] = useState({names:''})
-  // const handleChange = (e) =>{
-  //   console.log(e.target)
-  //   const {names,value} = e.target;
-  //   setFormData({...formData, [names]:value})
-  // }
+  const handleChange = (names,values) =>{
+    setFormData({...formData, [names]:values})
+    console.log(formData)
+  }
   const handleSubmit = (e) =>{
     e.preventDefault();
   }
   return (
       <div>
         <SideBar/>
-        Asden Maintenance 
+        <h1>Asden Maintenance</h1>
         <form onSubmit={handleSubmit}>
-          <TextFormComponent name="Input Name:"  isRequired={false} placeholder="Name"/>
+          <TextFormComponent name="Input Name:" handleChange={handleChange}  isRequired={false} placeholder="Name"/>
           <DropDownFormComponent name="Address"  isRequired={true} options={buildingAddressesArray}/>
           <TextFormComponent name="Input Apt:"  isRequired={true} placeholder="Apt #"/>
           <TextFormComponent name="Input Notes:"  isRequired={false} placeholder="Notes"/>
