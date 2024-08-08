b55255ba4db4a280439abe74481b8bf74096335b
Asden <edthevargas@gmail.com>
2024-08-08 15:39:23 -0400

    add new route

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.scss"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import IndexPage from "./pages/IndexPage.jsx"
-import IndexPage from "./pages/IndexPage.jsx";
 function App() {
    return (
       <BrowserRouter>
          <Routes>
             <Route path="/" element={<IndexPage />} />
+            <Route path="/data" element={<>hello</>} />
          </Routes>
       </BrowserRouter>
    );
