0539ff9d4ced91e3f3638b4c0964ad2b11f06fdc
Asden <edthevargas@gmail.com>
2024-08-08 16:20:39 -0400

    added sidebar to all routes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import IndexPage from "./pages/IndexPage.jsx"
+import SideBar from "./components/sidebar.jsx"
 function App() {
    return (
       <BrowserRouter>
+          <SideBar/>
          <Routes>
             <Route path="/" element={<IndexPage />} />
             <Route path="/data" element={<>hello</>} />
